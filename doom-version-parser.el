bcb9e33ff3ab36c985ed9d9f23c7dd05e9feb802
Justin Barclay <justincbarclay@gmail.com>
2019-01-10 13:20:44 -0500

    Add copyright and description of the main string

--- doom-version-parser.el
+++ doom-version-parser.el
@@ -1,4 +1,23 @@
 ;;; version-parser.el --- A version parser for doom-modeline -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 Justin Barclay
+
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
 (require 'subr-x)
 
 (defun doom-version-parser--ruby (line)
@@ -39,7 +58,10 @@
   (cadr
    (split-string line)))
 
-(defun doom-version-parser--get (prog callback args)
+(defun doom-version-parser--get (prog args callback)
+  "Starts a sub process using prog and applies the args to the sub process.
+   Once it recieves information from STDOUT, it closes off the subprcess and
+   passes on the information into the callback."
   (let ((proc (apply 'start-process
 		     (append ;; Flaten process-args into a single list so we can handle variadic length args
                       (list "doom-modeline-prog" "doom-modeline-prog" prog)
